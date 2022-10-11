c021b6f72ed02fd224c8e7a5c53f6b5b75d59e05
licinio.psj@gmail.com <110546077+liciniopsj@users.noreply.github.com>
2022-10-11 16:23:28 -0300

    Req 1 Clean up

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -24,8 +24,4 @@ describe('1 - Teste a função fetchProducts', () => {
     expect.assertions(1);
     expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
   });
-  // implemente seus testes aqui*
-  fail('Teste vazio');
 });
-
-// it('', () => {});
